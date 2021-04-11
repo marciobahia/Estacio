f41bc1c3b0dede251713d99c0a6ea3bf08e8418d
marciobahia <marciosellabahia@hotmail.com>
2021-04-11 20:27:50 -0300

    Add scripts with Threads, Process, Lock and Time

--- Python/src/script_anonima.py
+++ Python/src/script_anonima.py
@@ -5,6 +5,6 @@ def multiplica_por(multiplicador):
     print(multiplicar_por_10(1))
     print(multiplicar_por_10(2))
 
-    multiplicar_por_5 = multiplicar_por_5(5)
+    multiplicar_por_5 = multiplica_por(5)
     print(multiplicar_por_5(1))
     print(multiplicar_por_5(2))
