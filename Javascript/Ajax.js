112394a55a59c0fa58cc9c6058a97f4fb7bfa762
Marcio Bahia <marciosellabahia@hotmail.com>
2022-06-02 00:04:22 -0300

    Order

--- Javascript/Ajax.js
+++
@@ -1,4 +0,0 @@
-var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'https://api.github.com/users/marciobahia');
-xhr.send(null);
--- Javascript/Ajax/main.js
+++
@@ -1,5 +0,0 @@
-var marcio = new XMLHttpRequest();
-
-marcio.open('GET', 'https://api.github.com/users/marciobahia');
-marcio.send(null);
- 
